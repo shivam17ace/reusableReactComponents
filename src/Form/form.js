3b042b99edaf8f43553a166299b9ce7e62a24cc4
shivam <shivam17101@gmail.com>
2022-07-01 12:26:57 +0530

    first commit

---
+++ src/Form/form.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import './form.css';
+function Form(){
+    const [inputDate, setInputDate] = useState('');
+    const [inputName, setInputName] = useState('');
+    const [data, setData] = useState('')
+    const [display, setDisplay] = useState(false)
+    const inputDateHandler = (event) => {
+        setInputDate(event.target.value)
+    }
+
+    const inputNameHandler = (event) => {
+        setInputName(event.target.value)
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const info =[];
+        info.push(inputName,inputDate)
+        setData(info)
+    }
+    const handleShowData = () => {
+        const toggle = display ? false : true;
+        setDisplay(toggle)
+    }
+
+    return (
+        <div className="container">
+            <div className="input">
+                <div className="label">
+                    <label>Select Date</label>
+                </div>
+                <input type="date" onChange={inputDateHandler} />
+                <div className="label">
+                    <label>Enter Name</label>
+                </div>
+                <input type="text" onChange={inputNameHandler} />
+            </div>
+            <button type="submit" onClick={handleSubmit}>Submit</button>
+            <div>
+                <button type="submit" onClick={handleShowData}> Show Data </button>
+                {
+                    display ? data : null
+                }
+            </div>
+        </div>
+    )
+}
+export default Form;
